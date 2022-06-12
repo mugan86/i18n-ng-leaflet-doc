ec6e57405a4479f05c10a7031cc94b9e14b7f368
Anartz Mugika Ledo <mugan86@gmail.com>
2022-06-12 18:10:05 +0200

    fix: not add node_modules

--- ng-leaflet-documentation/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/anartz/Documents/librerias/angular/i18n-ng-leaflet-doc/ng-leaflet-documentation/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/anartz/Documents/librerias/angular/i18n-ng-leaflet-doc/ng-leaflet-documentation/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/anartz/Documents/librerias/angular/i18n-ng-leaflet-doc/ng-leaflet-documentation/node_modules/@docusaurus/theme-classic/lib/admonitions.css"),
-  require("/Users/anartz/Documents/librerias/angular/i18n-ng-leaflet-doc/ng-leaflet-documentation/src/css/custom.css"),
-];
